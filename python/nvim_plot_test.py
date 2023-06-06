0f703d1bdaa4ab1ffd998c9c0782c5db54fc8377
clpan <pancgl@outlook.com>
2023-06-05 18:23:56 -0600

    added emoji unicode

--- python/nvim_plot_test.py
+++ python/nvim_plot_test.py
@@ -1,3 +1,5 @@
+# test if plot can be shown
+# with global python, plot is shown in kitty terminal. Yet to test other env or other terminal emulators
 import matplotlib.pyplot as plt
 import numpy as np
 
--- tex/clean_up_tex.sh
+++ tex/clean_up_tex.sh
@@ -18,6 +18,7 @@ find . -type f -name "*.log" -delete
 find . -type f -name "*.bcf" -delete
 find . -type f -name "*.bbl" -delete
 find . -type f -name "*.blg" -delete
+find . -type f -name "*.out" -delete
 find . -type f -name "*.fls" -delete
 find . -type f -name "*.fdb_latexmk" -delete
 find . -type f -name "*.synctex.gz" -delete
